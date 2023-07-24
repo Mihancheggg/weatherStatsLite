c289bb480b0e6d36b33e94fac429207081c0b83e
Mihail Schutow <schutow.mihail@yandex.ru>
2023-07-24 09:32:58 +0300

    Add utils tests

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,1 +1,17 @@
-export {};
+import { dataFormatter, getAverageOfNumsArr } from './utils'
+
+describe('Utils tests', () => {
+    test('test data formatting', () => {
+        expect(dataFormatter(1)).toBe('01');
+        expect(dataFormatter(9)).toBe('09');
+        expect(dataFormatter(10)).toBe('10');
+        expect(dataFormatter(22)).toBe('22');
+    });
+
+    test('getting average fo nums arr', () => {
+        expect(getAverageOfNumsArr([1])).toBe(1);
+        expect(getAverageOfNumsArr([1,2])).toBe(2);
+        expect(getAverageOfNumsArr([1,2,3])).toBe(2);
+        expect(getAverageOfNumsArr([1,200])).toBe(101);
+    });
+})
